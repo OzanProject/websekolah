2d8d59eb8d3e394d7a50e0ba508db0b8c399fdf2
ozanproject <admin@admin.com>
2026-06-10 06:36:02 +0700

    UI: Tambahkan SweetAlert notifikasi pada halaman Admin Fasilitas

--- resources/views/admin/facilities/index.blade.php
+++ resources/views/admin/facilities/index.blade.php
@@ -125,8 +125,29 @@
 @stop
 
 @section('js')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
         $(document).ready(function() {
+            @if(session('success'))
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Berhasil!',
+                    text: '{{ session('success') }}',
+                    timer: 3000,
+                    showConfirmButton: false
+                });
+            @endif
+
+            @if(session('error'))
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Gagal!',
+                    text: '{{ session('error') }}',
+                    timer: 4000,
+                    showConfirmButton: false
+                });
+            @endif
+
             var table = $('#facilitiesTable').DataTable({
                 "paging": true,
                 "lengthChange": true,
